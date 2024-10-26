65be47bf9459121e7b3ffa8ac90aa38b37437388
rstickles16 <rstickles16@outlook.com>
2024-10-26 09:03:09 -0400

    commit

--- StratLab/xcel.py
+++ StratLab/xcel.py
@@ -5,4 +5,5 @@ def write_to_excel(
         df: pd.DataFrame,
         sheet_name: str = 'sheet1'
 ):
-    df.to_excel(writer, sheet_name=sheet_name)
+    with writer:
+        df.to_excel(writer, sheet_name=sheet_name)
