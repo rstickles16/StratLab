d5ba7a64070419bf3ed0b0e9cb7476170fcd1b69
rstickles16 <rstickles16@outlook.com>
2024-12-08 11:07:43 -0500

    commit

--- StratLab/tests/_test.py
+++ StratLab/tests/_test.py
@@ -5,7 +5,20 @@ import StratLab
 
 class TestClass:
 
-    def test_import(self):
+    def test_sma_200(self):
         bt = StratLab.Backtest()
-    
-    
+        bt.add_condition(
+            name='long_sma',
+            ticker_1='qqq',
+            study_1='price',
+            operator='>',
+            ticker_2='qqq',
+            study_2='sma',
+            study_2_period=200
+        )
+
+        bt.add_holding(
+            ['long_sma'], [True], ['qqq']
+        )
+
+        bt.run()
